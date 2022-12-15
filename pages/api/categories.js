f8b4b3745e9fd611b72361582f5bb66db48facc7
phung12017 <tranthanhpphung092015@example.com>
2022-12-15 20:20:31 +0700

    npm i nextjs-cors

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,6 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import NextCors from 'nextjs-cors';
 
 export default function handler(req, res) {
+
+  await NextCors(req, res, {
+    // Options
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    origin: '*',
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+ });
+
   res.status(200).json({ "categories":[
 
     {
