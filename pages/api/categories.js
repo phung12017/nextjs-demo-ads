ddac341ae11360b806bab5878e98eb0fe04afcaf
phung12017 <tranthanhpphung092015@example.com>
2022-12-15 20:24:50 +0700

    asyn

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import NextCors from 'nextjs-cors';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
   await NextCors(req, res, {
     // Options
